6029309e8328d904eb3349769ad9f6c05fb66ac2
Cartmade2019 <support@cartmade.com>
2024-05-09 17:32:16 +0545

    updated alignments

--- app/routes/components/Column/Column.jsx
+++ app/routes/components/Column/Column.jsx
@@ -6,18 +6,22 @@ import {Text} from '@shopify/polaris';
 
 export const Column = ({ tasks,onRemove }) => {
     return (
-    <div className="column">
-        <Text variant="headingXl" as="h4">
+        <div className="column-wrap">
+            <Text variant="headingXl" as="h4">
             Selected Filters
         </Text>
         <Text variant="headingXs" as="h6">
              Manage your selected filters.
         </Text>
+        
+    <div className="column">
+        
         <SortableContext items={tasks} strategy={verticalListSortingStrategy} distance={1}>
             {tasks.map((task)=>(
                 <Task id={task.id} title={task.title} key={task.id} metakey={task.metakey} onRemove={onRemove}/>
             ))}
         </SortableContext>
+    </div>
     </div>
     )
 };
